fefb43ce3bc7e001c7c305f2edbf3a41f21ab862
Pietro Ferretti <pietro.ferretti1@gmail.com>
2018-03-20 15:25:40 +0100

    Added default error handlers

--- webapp/app.py
+++ webapp/app.py
@@ -33,6 +33,33 @@ def build_response(content, restype='json', error=None):
         raise ValueError('Response type {} not recognized'.format(restype))
 
 
+# default error handlers
+# TODO add hypermedia to error handlers
+@app.errorhandler(400)
+def bad_request(e):
+    return build_response(None, error='Bad Request'), 400
+
+@app.errorhandler(401)
+def authorization_required(e):
+    return build_response(None, error='Authorization Required'), 401
+
+@app.errorhandler(403)
+def forbidden(e):
+    return build_response(None, error='Forbidden'), 403
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return build_response(None, error='Page Not Found'), 404
+
+@app.errorhandler(405):
+def method_not_allowed(e):
+    return build_response(None, error='Method Not Allowed'), 405
+
+@app.errorhandler(500)
+def server_error(e):
+    return build_response(None, error='Internal Server Error'), 500
+
+
 @app.route('/')
 def index():
     '''Root endpoint'''
